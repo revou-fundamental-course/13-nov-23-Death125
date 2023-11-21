5117e948c7576fa233c18c8235d217e94eb34716
Viki Fernando <viki.fernando2@gmail.com>
2023-11-21 20:48:24 +0700

    add javascript validity

--- js/script.js
+++ js/script.js
@@ -19,3 +19,37 @@ function slider() {
   img.innerHTML = "<img src=" + slides[Start - 1] + ">";
 }
 setInterval(slider, 2000);
+
+function validateForm() {
+  const name = document.forms["form-input"]["name-input"].value;
+  const email = document.forms["form-input"]["email-input"].value;
+  const option = document.forms["form-input"]["select-input"].value;
+
+  if (name == "") {
+    document.getElementById("error-name").innerHTML = "Name cannot be empty";
+    document.getElementById("error-name").style.color = "crimson";
+  } else {
+    clearField();
+  }
+
+  if (email == "") {
+    document.getElementById("error-email").innerHTML = "Email cannot be empty";
+    document.getElementById("error-email").style.color = "crimson";
+  } else {
+    clearField();
+  }
+
+  if (option == "") {
+    document.getElementById("error-select").innerHTML = "You must select one";
+  } else {
+    clearField();
+  }
+
+  return false;
+}
+
+function clearField() {
+  document.getElementById("error-name").innerHTML = "";
+  document.getElementById("error-email").innerHTML = "";
+  document.getElementById("error-select").innerHTML = "Select option";
+}
