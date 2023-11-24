e668f2a45c8b989dad73c10273991854fc000304
Viki Fernando <viki.fernando2@gmail.com>
2023-11-24 14:13:09 +0700

    add dialog box, tidy up layout

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var img = document.getElementById("img");
 
-var slides = [
+var images = [
   "assets/Bali.png",
   "assets/Japan.png",
   "assets/Singapore.png",
@@ -9,16 +9,16 @@ var slides = [
   "assets/Amerika.png",
 ];
 
-var Start = 0;
+var startImage = 0;
 
 function slider() {
-  if (Start < slides.length) {
-    Start = Start + 1;
+  if (startImage < images.length) {
+    startImage = startImage + 1;
   } else {
-    Start = 1;
+    startImage = 1;
   }
-  console.log(img);
-  img.innerHTML = "<img src=" + slides[Start - 1] + ">";
+
+  img.innerHTML = "<img src=" + images[startImage - 1] + ">";
 }
 setInterval(slider, 2000);
 
@@ -27,30 +27,36 @@ function validateForm() {
   const email = document.forms["form-input"]["email-input"].value;
   const option = document.forms["form-input"]["select-input"].value;
 
-  if (name == "") {
-    document.getElementById("error-name").innerHTML = "Name cannot be empty";
-    document.getElementById("error-name").style.color = "crimson";
-  } else {
-    document.getElementById("error-name").innerHTML = "";
-  }
+  if (name == "" || email == "" || option == "") {
+    if (name == "") {
+      document.getElementById("error-name").innerHTML = "Name cannot be empty";
+      document.getElementById("error-name").style.color = "crimson";
+    } else {
+      document.getElementById("error-name").innerHTML = "";
+    }
 
-  if (email == "") {
-    document.getElementById("error-email").innerHTML = "Email cannot be empty";
-    document.getElementById("error-email").style.color = "crimson";
-  } else {
-    document.getElementById("error-email").innerHTML = "";
-  }
+    if (email == "") {
+      document.getElementById("error-email").innerHTML =
+        "Email cannot be empty";
+      document.getElementById("error-email").style.color = "crimson";
+    } else {
+      document.getElementById("error-email").innerHTML = "";
+    }
 
-  if (option == "") {
-    document.getElementById("error-select").innerHTML =
-      "Selection cannot be empty";
-    document.getElementById("error-select").style.color = "crimson";
-  } else {
-    document.getElementById("error-select").innerHTML = "";
-    document.getElementById("default-select").innerHTML = "Select Option";
-  }
+    if (option == "") {
+      document.getElementById("error-select").innerHTML =
+        "Selection cannot be empty";
+      document.getElementById("error-select").style.color = "crimson";
+    } else {
+      document.getElementById("error-select").innerHTML = "";
+      document.getElementById("default-select").innerHTML = "Select Option";
+    }
 
-  return false;
+    return false;
+  } else {
+    clearField();
+    alert("Terima kasih sudah berlangganan kepada kami 😁");
+  }
 }
 
 function clearField() {
